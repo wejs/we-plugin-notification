3aca9bf5d08418f7386911fac3638b314da23d2f
albertosouza <alberto.souza.99@gmail.com>
2015-02-04 12:14:15 -0200

    fix notification text

--- client/app/models/NotificationModel.js
+++ client/app/models/NotificationModel.js
@@ -1,42 +1,44 @@
 
-// MODEL
-App.Notification = DS.Model.extend({
-  user: DS.attr('string'),
-  text: DS.attr('string'),
-  textClean: DS.attr('string'),
+$(function() {
+  // set default notifications attrs
+  App.Notification.reopen({
+    user: DS.attr('string'),
+    text: DS.attr('string'),
+    textClean: DS.attr('string'),
 
-  actor: DS.belongsTo('user', {async: true}),
+    actor: DS.belongsTo('user', {async: true}),
 
-  model: DS.attr('string'),
-  action: DS.attr('string'),
-  modelId: DS.attr('string'),
+    model: DS.attr('string'),
+    action: DS.attr('string'),
+    modelId: DS.attr('string'),
 
-  relatedModel: DS.attr(),
+    relatedModel: DS.attr(),
 
-  targetModelId: DS.attr('string'),
-  targetModelType: DS.attr('string'),
+    targetModelId: DS.attr('string'),
+    targetModelType: DS.attr('string'),
 
-  displayName: DS.attr('string'),
-  locale: DS.attr('string'),
-  contentType: DS.attr('string'),
-  groupName: DS.attr('string'),
-  groupId: DS.attr('string'),
-  relatedContentTeaser: DS.attr('string'),
+    displayName: DS.attr('string'),
+    locale: DS.attr('string'),
+    contentType: DS.attr('string'),
+    groupName: DS.attr('string'),
+    groupId: DS.attr('string'),
+    relatedContentTeaser: DS.attr('string'),
 
-  // flag used to create the notification
-  flagType: DS.attr('string'),
+    // flag used to create the notification
+    flagType: DS.attr('string'),
 
-  notified: DS.attr('boolean',{
-    defaultValue: false
-  }),
+    notified: DS.attr('boolean',{
+      defaultValue: false
+    }),
 
-  status: DS.attr('string'),
+    status: DS.attr('string'),
 
-  read: DS.attr('boolean',{
-    defaultValue: false
-  }),
+    read: DS.attr('boolean',{
+      defaultValue: false
+    }),
 
-  createdAt: DS.attr('date')
+    createdAt: DS.attr('date')
+  });
+
+  App.NotificationAdapter = App.ApplicationRESTAdapter.extend();
 });
-
-App.NotificationAdapter = App.ApplicationRESTAdapter.extend();
